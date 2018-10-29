789e93cb817c5cc52b73166abb8a7ace33a75a39
Philip Devine <devine.pk@gmail.com>
2018-10-29 14:34:39 -0400

    adds home link to nav image. adds field and file links to addnewloveit

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -1,17 +1,6 @@
-<!-- Image and text -->
 <nav class="navbar navbar-light bg-white border-bottom">
-  <a class="navbar-brand" href="#">
+  <a class="navbar-brand" href="/">
     <img src="images/I.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
     LoveIt
   </a>
 </nav>
-
-
-
-<!-- <div class="text-center">
-  <nav class="navbar-brand mb-0 h1">
-      <span class="navbar-text mr-2">I</span>
-        <img src="images/I.jpg" width="30" height="30" class="d-inline-block align-top" alt=""></a>
-      <span class="navbar-text ml-2">It When</span>
-  </nav>
-</div> -->
--- resources/views/pages/addLoveIt.blade.php
+++ resources/views/pages/addLoveIt.blade.php
@@ -1,1 +1,27 @@
 @extends('layouts')
+
+@section('title')
+  Add a new LoveIt
+@endsection
+
+@section('content')
+<div class=container>
+
+  <form>
+    <div class="form-group">Write about it...
+      <textarea class="form-control mt-2 mb-5" id="exampleFormControlTextarea1" rows="3">What do you love?</textarea>
+    </div>
+    <div class="form-group mb-3">
+      <label for="exampleFormControlFile1">Or take a picture...</label>
+      <input type="file" class="form-control-file" id="fileUpload">
+    </div>
+  </form>
+
+<div class="text-center">
+  <button type="submit" class="mt-2 btn btn-primary">Share</button>
+</div>
+
+</div>
+@endsection
+
+@extends('footer')
