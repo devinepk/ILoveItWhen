37c19ec85ed8eec83bad382f2aaba49f860ec2b1
Philip Devine <devine.pk@gmail.com>
2018-10-30 15:14:28 -0400

    almost finished profile view.

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -9,61 +9,51 @@
 
 <div class="container">
   <div class="row">
-    <div class="col">
-      <img src="/images/profilepic.jpg" class="img-fluid" width="100" height="auto" alt="Responsive image">
-      <small class="form-text text-muted">Hipster_Dad</small>
+    <div class="col" id="profilePic">
+      <img src="/images/profilepic.jpg" class="img-fluid" width="125" height="auto" alt="Responsive image">
+      <small class="form-text text-muted" id="profileName">Hipster_Dad</small>
     </div>
     <div class="col text-center">
-     <p>8</p>
-     <p>Number of Posts</p>
-     <button type="button" class="btn btn-primary btn-sm">Edit Profile</button>
-
-      </div>
-
+       <h2 id="postCount">8</h2>
+       <p class="text-muted">Number of Posts</p>
+       <button type="button" class="btn btn-primary btn-sm mt-2">Edit Profile</button>
     </div>
   </div>
 
 
 </br>
-<p>Family man & coffee drinker. Hipster before it was cool.  Now just cool.</p>
-</br>
-</div>
-<div class="container">
-  <div class="row">
-    <div class="col">
-      One of three columns
-    </div>
-    <div class="col">
-      One of three columns
-    </div>
-    <div class="col">
-      One of three columns
-    </div>
-  </div>
-  <div class="row">
-    <div class="col">
-      One of three columns
-    </div>
-    <div class="col">
-      One of three columns
-    </div>
-    <div class="col">
-      One of three columns
-    </div>
-  </div>
-  <div class="row">
-    <div class="col">
-      One of three columns
-    </div>
-    <div class="col">
-      One of three columns
-    </div>
-    <div class="col">
-      One of three columns
-    </div>
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <p class="lead">Family man & coffee drinker. Hipster before it was cool.  Now just cool.</p>
   </div>
 </div>
 
+
+
+
+<div class="row mb-1">
+  <div class="col text-center mr-0" id="profilePictures">
+    <img src="/images/profilepic.jpg" class="img-fluid" width="100" height="auto" alt="Responsive image">
+    <img src="/images/profilepic.jpg" class="img-fluid" width="100" height="auto" alt="Responsive image">
+    <img src="/images/profilepic.jpg" class="img-fluid" width="100" height="auto" alt="Responsive image">
+  </div>
+</div>
+<div class="row mb-1">
+  <div class="col text-center mr-0">
+    <img src="/images/profilepic.jpg" class="img-fluid" width="100" height="auto" alt="Responsive image">
+    <img src="/images/profilepic.jpg" class="img-fluid" width="100" height="auto" alt="Responsive image">
+    <img src="/images/profilepic.jpg" class="img-fluid" width="100" height="auto" alt="Responsive image">
+  </div>
+</div>
+<div class="row mb-1">
+  <div class="col text-center mr-0">
+    <img src="/images/profilepic.jpg" class="img-fluid" width="100" height="auto" alt="Responsive image">
+    <img src="/images/profilepic.jpg" class="img-fluid" width="100" height="auto" alt="Responsive image">
+    <img src="/images/profilepic.jpg" class="img-fluid" width="100" height="auto" alt="Responsive image">
+  </div>
+</div>
+</div>
+</div>
 @endsection
 
 
