7d4bc2d9b3aec906b5c583d26e16cabedc62ab68
Philip Devine <devine.pk@gmail.com>
2018-11-12 21:12:10 -0500

    corrects links on landing page to reflect authorization framework.

--- resources/views/splash.blade.php
+++ resources/views/splash.blade.php
@@ -6,20 +6,19 @@
 
 @section('content')
 <div class="container text-center">
-<h1 class="mt-5 mb-5">I</h1>
+<h1 class="mt-3 mb-3">I</h1>
 
 <img src="images/I.jpg" alt="Heart" class="w-25">
 
-<h1 class="mt-5 mb-5">it when...</h1>
+<h1 class="mt-3 mb-5">it when...</h1>
 
-  <a href="/new"><button type="submit" class="mt-2 btn btn-outline-primary">Enter</button></a>
+  <a href="/login"><button type="submit" class="mt-2 btn btn-outline-primary">Sign In</button></a>
 
 </div>
-@endsection
 
-@section('footer')
-<div class="mt-5 container text-center">
+
+<div class="mt-3 container text-center">
 <p><a href="/public">See what other people love</a></p>
-<p><a href="/login">Don't have an account? Create one here</a></p>
+<p><a href="/register">Don't have an account? Create one here</a></p>
 </div>
 @endsection
