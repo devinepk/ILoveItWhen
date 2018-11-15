7f50d2b4005c9383f44535177d500338f2f7a9a5
Philip Devine <devine.pk@gmail.com>
2018-11-15 11:58:05 -0500

    changes routing after new registration to send user to add new post page.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/new';
 
     /**
      * Create a new controller instance.
