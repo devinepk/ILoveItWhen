f471bce67f9b7d0d885b30206c6872811fa63887
Philip Devine <devine.pk@gmail.com>
2018-11-27 16:07:15 -0500

    builds pivot table in spaces_users migration.

---
+++ database/migrations/2018_11_27_154414_spaces_users.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SpacesUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('spaces_users', function (Blueprint $table) {
+        $table->increments('id');
+        $table->unsignedInteger('space_id');
+        $table->foreign('space_id')->references('id')->on('spaces');
+        $table->unsigned('user_posts_id');
+        $table->foreign('user_posts_id')->references('id')->on('user_posts');
+        $table->unsigned('users_id');
+        $table->foreign('users_id')->references('id')->on('users');
+        $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('spaces_users');
+    }
+}
