63b4e33cb64f227e892609d2b7cd858f5a5606e9
Philip Devine <devine.pk@gmail.com>
2018-12-04 09:59:06 -0500

    space difference in editor.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,18 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                      <!-- <li class="nav-item">
+                         <a class="nav-link" href="/private">My Posts</a>
+                      </li>
+                      <li class="nav-item">
+                         <a class="nav-link" href="/spaces">My Spaces</a>
+                      </li>
+                      <li class="nav-item">
+                         <a class="nav-link" href="/">Add New Post</a>
+                      </li>
+                      <li class="nav-item">
+                         <a class="nav-link" href="/addnew">Add New Space</a>
+                      </li> -->
                     </ul>
 
                     <!-- Right Side Of Navbar -->
